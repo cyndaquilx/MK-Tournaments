2fe965aa15ef94114f2c5d56ad164f47ac0547ed
cyndaquilx <ultramk8@gmail.com>
2021-12-29 16:09:02 -0500

    fixed main file

--- organizerbot.py
+++ organizerbot.py
@@ -14,7 +14,7 @@ bot = commands.Bot(command_prefix='!', case_insensitive=True, intents=intents)
 
 initial_extensions = ['cogs.TournamentManager', 'cogs.Tables',
                       'cogs.Registration', 'cogs.Results',
-                      'cogs.TeamManagement']
+                      'cogs.TeamManagement', 'cogs.MKCentral']
 
 with open('./config.json', 'r') as cjson:
     bot.config = json.load(cjson)
