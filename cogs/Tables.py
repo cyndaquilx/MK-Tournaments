cf5cc58fb04f44960486ff829fa6d86c4222b118
cyndaquilx <ultramk8@gmail.com>
2024-07-08 13:48:25 -0400

    make table submit automatically after 30 sec, change some strings

--- cogs/Tables.py
+++ cogs/Tables.py
@@ -34,7 +34,9 @@ class Tables(commands.Cog):
                 return
             table = tRound.rooms[roomNum-1].table
         sb = table.scoreboard()
-        msg = f"```{sb}```\nPaste the above into https://hlorenzi.github.io/mk8d_ocr/table.html for the table."
+        msg = f"```!submit {roomNum}\n{sb}```" + \
+        "\nPaste the above into https://hlorenzi.github.io/mk8d_ocr/table.html for the table." + \
+        "\nReplace the 0s with each player's score and use the `!submit` command to submit the table."
         await ctx.send(msg)
 
     async def tableEmbed(self, ctx, tournament, tround, room, data):
@@ -128,6 +130,7 @@ class Tables(commands.Cog):
         X_MARK = "\U0000274C"
         await embedded.add_reaction(CHECK_BOX)
         await embedded.add_reaction(X_MARK)
+        submitted_msg = f"{ctx.author.mention} Successfully submitted table for room {roomid}! If there are any errors, you can fix them by using the `!submit` command again."
 
         def check(reaction, user):
             if user != ctx.author:
@@ -141,7 +144,10 @@ class Tables(commands.Cog):
         try:
             reaction, user = await self.bot.wait_for('reaction_add', timeout=30.0, check=check)
         except:
+            room.updateTable(tournament, players, scores)
             await embedded.delete()
+            await ctx.send(submitted_msg)
+            await self.sendResults(ctx, roomid, e)
             return
 
         if str(reaction.emoji) == X_MARK:
@@ -149,7 +155,7 @@ class Tables(commands.Cog):
             return
         room.updateTable(tournament, players, scores)
         await embedded.delete()
-        await ctx.send("Table updated")
+        await ctx.send(submitted_msg)
         await self.sendResults(ctx, roomid, e)
 
     @commands.command(aliases=['fix'])
@@ -167,62 +173,7 @@ class Tables(commands.Cog):
             return
         room = lastRound.rooms[roomid-1]
         e, f, players, scores = await self.tableEmbed(ctx, tournament, lastRound, room, data)
-##        names, scores = parsing.parseLorenzi(data)
-##        pNum = tournament.playersPerRoom
-##        if len(names) != pNum:
-##            await ctx.send(f"Your table does not contain {pNum} valid score lines, try again!")
-##            return
-##        if len(set(names)) != len(names):
-##            await ctx.send("Duplicate names are not allowed! Try again")
-##            return
-##        players = room.getPlayersFromMiiNames(names)
-##        err_str = ""
-##        for i in range(len(players)):
-##            if players[i] is None:
-##                if len(err_str) == 0:
-##                    err_str += f"The following players cannot be found in Room {room.roomNum}:\n"
-##                err_str += f"{names[i]}\n"
-##        if len(err_str) > 0:
-##            await ctx.send(err_str)
-##            return
-##        sb = room.sampleScoreboard(players, scores)
-##        
-##        
-##        base_url_lorenzi = "https://gb.hlorenzi.com/table.png?data="
-##        url_table_text = urllib.parse.quote(sb)
-##        image_url = base_url_lorenzi + url_table_text
-##
-##        async with aiohttp.ClientSession() as session:
-##            async with session.get(image_url) as resp:
-##                if resp.status != 200:
-##                    f = None
-##                    imgData = None
-##                else:
-##                    imgData = io.BytesIO(await resp.read())
-##                    f = discord.File(imgData, filename="MogiTable.png")
-##        
-##        e = discord.Embed(title="Table")
-##        adv, tie, extra = room.checkAdvanced(tournament, players, scores)
-##              
-##        if len(adv) > 0:
-##            advMsg = ""
-##            for team in adv:
-##                advMsg += f"{team.tableName()}\n"
-##            e.add_field(name="Advancing", value=advMsg)
-##        if len(tie) > 0:
-##            tieMsg = ""
-##            for team in tie:
-##                tieMsg += f"{team.tableName()}\n"
-##            e.add_field(name="Tiebreaker", value=tieMsg, inline=False)
-##        if len(extra) > 0:
-##            extraMsg = ""
-##            n = tournament.getNthPlace()
-##            for team in extra:
-##                extraMsg += f"{team.team.tableName()}\n"
-##            e.add_field(name=f"{n} Place Teams", value=extraMsg,inline=False)
-##        
-##        e.set_image(url="attachment://MogiTable.png")
-        #e.set_image(url=image_url)
+
         content = f"{ctx.author.mention} **WARNING:** The rooms in the current round may be affected by this change. Make sure this is okay before confirming"
         embedded = await ctx.send(file=f, content=content, embed=e)
         #ballot box with check emoji
@@ -266,7 +217,6 @@ class Tables(commands.Cog):
             msg = "The following changes have been made to the current round:\n" + msg
             await ctx.send(msg)
         await self.sendResults(ctx, roomid, e, lastRound.roundNum)
-        #await ctx.send("Table updated")
         
 
     #@commands.command()
